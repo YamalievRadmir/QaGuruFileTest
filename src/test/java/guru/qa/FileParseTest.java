6c4462d7535955c1f2b79b3c34f90a0f7cbf8855
Radmir <yaradmir2203@gmail.com>
2022-06-19 22:50:23 +0500

    Add test1

--- src/test/java/guru/qa/FileParseTest.java
+++ src/test/java/guru/qa/FileParseTest.java
@@ -2,14 +2,14 @@ package guru.qa;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.opencsv.CSVReader;
+import guru.qa.domain.Staff;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -52,22 +52,15 @@ public class FileParseTest {
             List<String[]> content = csv.readAll();
             assertThat(content.get(2)).contains("1;Креветки в кляре");
         }
-
-
-        //@Test
-        //void jsonTest() {
-        //InputStream is = classLoader.getResourseAsStream("card.json");
-        //Gson gson = new Gson();
-       // JsonObject jsonObject = gson.fromJson(new InputStreamReader(is), JsonObject.class);
-        //assertThat(jsonObject.get("name").getAsString()).isEqualTo("Radmir");
-        //assertThat(jsonObject.get("isGoodStudent").getAsBoolean()).isEqualTo(true);
-       // }
+    }
 
         @Test
         void parseJsonTest() throws Exception{
             ObjectMapper mapper = new ObjectMapper();
             Staff staff = mapper.readValue(Paths.get(jsonClassPath).toFile(), Staff.class);
             assertThat(staff.name).isEqualTo("Radmir");
+            assertThat(staff.isGoodStudent).isEqualTo("true");
+            assertThat(staff.age).isEqualTo("28");
     }
 }
 
---
+++ src/test/java/guru/qa/domain/Staff.java
@@ -0,0 +1,9 @@
+package guru.qa.domain;
+
+public class Staff {
+        public String name;
+        public String isGoodStudent;
+        public String age;
+    }
+
+
